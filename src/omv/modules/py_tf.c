6c00d6c358d1d263e5b01ea10502e2cba2b32611
tejalbarnwal <tejalbarnwal@gmail.com>
2022-10-18 03:00:49 +0530

    Debug: output data2

--- src/omv/modules/py_tf.c
+++ src/omv/modules/py_tf.c
@@ -325,9 +325,10 @@ STATIC void py_tf_regression_output_callback(void *callback_data,
     if (params->output_datatype == LIBTF_DATATYPE_INT8) {
         for (int i = 0, ii = params->output_channels; i < ii; i++) {
 
-            mp_float_t temp = (float)( ((float) (((int8_t *) model_output)[i] - params->output_zero_point)) * params->output_scale);
+            mp_float_t temp = (float) (((int8_t *) model_output)[i]);
             printf( "raw output %d \t", (int)temp);
-            // printf( "scaled zeroed output %d \t", (int)((temp - params->output_zero_point) * params->output_scale) );
+            mp_float_t up_temp = (float) ( (temp - params->output_zero_point) * params->output_scale);
+            printf( "scaled zeroed output %d \t", (int)up_temp);
 
             ((mp_obj_list_t *) arg->out)->items[i] =
                     mp_obj_new_float( ((float) (((int8_t *) model_output)[i] - params->output_zero_point)) * params->output_scale);
