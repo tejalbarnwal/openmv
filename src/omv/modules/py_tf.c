40cf9cc1da31f693f22c573dac77908bf187a069
tejalbarnwal <tejalbarnwal@gmail.com>
2022-12-24 11:48:36 +0530

    Debug: tejuinput func input

--- src/omv/modules/py_tf.c
+++ src/omv/modules/py_tf.c
@@ -431,7 +431,10 @@ STATIC mp_obj_t py_tf_tejuinput(uint n_args, const mp_obj_t *args, mp_map_t *kw_
     float arr[36];
     mp_obj_list_t *arg_list = args[1];
     for(size_t i =0; i < arg_list->len; i++){
+        printf("original \n");
+        printf("%f \n", (double) mp_obj_float_get(arg_list->items[i]));
         arr[i] = (float) mp_obj_float_get(arg_list->items[i]);
+        printf("intoarr \n");
         printf("%f \n", (double) arr[i]);
     }
 
