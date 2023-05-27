fef627c20cb338b740f86def72fc7a1afc318bd2
tejalbarnwal <tejalbarnwal@gmail.com>
2023-05-27 15:04:32 +0530

    verify read input is the correct input

--- src/omv/modules/py_tf.c
+++ src/omv/modules/py_tf.c
@@ -295,6 +295,7 @@ STATIC mp_obj_t py_tf_regression(uint n_args, const mp_obj_t *args, mp_map_t *kw
         for (size_t j=0; j<input_size_width; j++)
         {
             float *array = (float *)arg_input_array->array;
+            printf("At %u, %u \t :", i, j);
             printf("%f \n", (double) array[i][j]);
             // input_array[i][j] = (float) mp_obj_float_get(
             //     mp_binary_get_val_array(arg_input_array->typecode, arg_input_array->items, i)
